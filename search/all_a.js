b1b346fc947946bd8251e38482e763b672ce4bb9
lizabelos <belos.liza@gmail.com>
2023-10-27 13:27:09 +0000

    Deploying to gh-pages from @ lizabelos/UnivKbd@10d26e97132c01eebe13e2d0dc8518a9c77c6b88 🚀

--- search/all_a.js
+++ search/all_a.js
@@ -4,5 +4,5 @@ var searchData=
   ['virtualkeyboardbutton_31',['VirtualKeyboardButton',['../classUnivKbd_1_1VirtualKeyboardButton.html',1,'UnivKbd']]],
   ['virtualkeyboardconfigurationwidget_32',['VirtualKeyboardConfigurationWidget',['../classUnivKbd_1_1VirtualKeyboardConfigurationWidget.html',1,'UnivKbd']]],
   ['virtualkeyboardinnerwidget_33',['VirtualKeyboardInnerWidget',['../classUnivKbd_1_1VirtualKeyboardInnerWidget.html',1,'UnivKbd::VirtualKeyboardInnerWidget'],['../classUnivKbd_1_1VirtualKeyboardInnerWidget.html#a27fc3c932739b93322934615e974265c',1,'UnivKbd::VirtualKeyboardInnerWidget::VirtualKeyboardInnerWidget()']]],
-  ['virtualkeypressed_34',['virtualKeyPressed',['../classUnivKbd_1_1VirtualKeyboardInnerWidget.html#aa7978e2d08a682c67952283e187c18ff',1,'UnivKbd::VirtualKeyboardInnerWidget']]]
+  ['virtualkeypressed_34',['virtualKeyPressed',['../classUnivKbd_1_1VirtualKeyboardInnerWidget.html#aa612a142296fef90eb9fc9adf50cd78d',1,'UnivKbd::VirtualKeyboardInnerWidget']]]
 ];
